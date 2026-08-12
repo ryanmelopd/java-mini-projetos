5a8509621a99e716639d818163b25eeeeec22c0d
Ryan <ryanmelopedro190@gmail.com>
2026-08-12 17:25:24 -0300

    refactor: ajusta fluxo do combate

--- src/SistemaDeRPG/Main.java
+++ src/SistemaDeRPG/Main.java
@@ -10,57 +10,40 @@ public class Main {
 
         System.out.println("Informações do primeiro personagem:");
         player1.mostrarInformacoes();
-
         System.out.println();
-
         System.out.println("Informações do segundo personagem:");
         player2.mostrarInformacoes();
-
         System.out.println();
-
         System.out.println("Informações do monstro:");
         monstro1.mostrarInformacoes();
-
         System.out.println();
-
         System.out.println("Começou um combate entre " + player1.getNome() + " e " + player2.getNome() + " contra " + monstro1.getNome() + "!");
 
         // Turno do Leandro
         System.out.println("\nTurno de " + player1.getNome() + ":");
-
         player1.atacarMonstro(monstro1);
-
         System.out.println(player1.getNome() + " atacou " + monstro1.getNome() + ".");
-
         System.out.println(monstro1.getNome() + " está com " + monstro1.getVida() + " de vida.");
 
         // Turno do monstro
         System.out.println("\nTurno de " + monstro1.getNome() + ":");
-
         monstro1.atacar(player1);
-
         System.out.println(monstro1.getNome() + " atacou " + player1.getNome() + ".");
-
         System.out.println(player1.getNome() + " está com " + player1.getVida() + " de vida.");
 
         // Cura da Ana
         System.out.println("\nTurno de " + player2.getNome() + ":");
-
         player2.curar(player1);
-
         System.out.println(player2.getNome() + " curou " + player1.getNome() + ".");
-
         System.out.println(player1.getNome() + " está com " + player1.getVida() + " de vida.");
 
         // Verificação de derrota
         if (!monstro1.estaVivo()) {
             System.out.println(monstro1.getNome() + " foi derrotado!");
         }
-
         if (!player1.estaVivo()) {
             System.out.println(player1.getNome() + " foi derrotado!");
         }
-
         if (!player2.estaVivo()) {
             System.out.println(player2.getNome() + " foi derrotada!");
         }
