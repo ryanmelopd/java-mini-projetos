812fb1cc2772c0f3c7b9bd88ab79786c3ddb157f
Ryan <ryanmelopedro190@gmail.com>
2026-08-14 17:16:41 -0300

    feat: adiciona enum TipoConta ao sistema bancário

--- src/SistemaBancario/ContaFisica.java
+++ src/SistemaBancario/ContaFisica.java
@@ -2,10 +2,12 @@ package SistemaBancario;
 
 public class ContaFisica extends Conta {
     private String cpf;
+    private TipoConta tipoConta;
 
-    public ContaFisica(String nome, int numero, double saldo, String cpf) {
+    public ContaFisica(String nome, int numero, double saldo, String cpf, TipoConta tipoConta) {
         super(nome, numero, saldo);
         this.cpf = cpf;
+        this.tipoConta = tipoConta;
     }
 
     public String getcpf() {
@@ -21,5 +23,6 @@ public class ContaFisica extends Conta {
         System.out.println("Número da conta: " + getNumero());
         System.out.println("Saldo: " + getSaldo());
         System.out.println("CPF: " + cpf);
+        System.out.println("Tipo de conta: " + tipoConta.getTipoDeConta());
     }
 }
--- src/SistemaBancario/Main.java
+++ src/SistemaBancario/Main.java
@@ -10,13 +10,31 @@ public class Main {
         String nome = input.nextLine();
         System.out.print("Conta PJ ou Física: ");
         String opcaoConta = input.nextLine().toLowerCase();
+        System.out.print("Conta corrente ou poupança: ");
+        String opcaoTipoDeConta = input.nextLine().toLowerCase();
+        System.out.println();
+
+        TipoConta tipoConta;
+
+        if (opcaoTipoDeConta.equals("corrente")) {
+            tipoConta = TipoConta.CONTA_CORRENTE;
+        } else if (opcaoTipoDeConta.equals("poupanca") || opcaoTipoDeConta.equals("poupança")) {
+            tipoConta = TipoConta.CONTA_POUPANCA;
+        } else {
+            System.out.println("Tipo de conta inválido.");
+            return;
+        }
 
         if (opcaoConta.equals("pj")) {
-            ContaPj contaP1 = new ContaPj(nome,517490,0,"12.345.678/0001-95");
-            contaP1.mostrarDados();
+            ContaPj contaPj = new ContaPj(nome, 517490, 0, "12.345.678/0001-95", tipoConta);
+            contaPj.mostrarDados();
+        } else if (opcaoConta.equals("fisica") || opcaoConta.equals("física")) {
+            ContaFisica contaFisica = new ContaFisica(nome, 523123, 0, "111.444.777-35", tipoConta);
+            contaFisica.mostrarDados();
         } else {
-            ContaFisica contaF1 = new ContaFisica(nome,567890,0,"529.982.247-25");
-            contaF1.mostrarDados();
+            System.out.println("Conta inválida");
         }
+        input.close();
     }
 }
+
